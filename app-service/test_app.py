e1d72a698d51c8bb9533b57afa58b40023625821
devopsDanW <dwdanwu@outlook.com>
2026-09-02 21:16:38 +1000

    fix: add hadolint step to CI

--- app-service/test_app.py
+++ app-service/test_app.py
@@ -4,10 +4,10 @@ from app import app
 client = app.test_client()
 
 response = client.get("/healthz")
-assert response.status_code == 200, f"/healthz returned {response.status.code}"
+assert response.status_code == 200, f"/healthz returned {response.status_code}"
 assert response.get_json()["status"] == "healthy"
 
 response = client.get("/")
-assert response.status_code == 200, f"/ returned {response.status.code}"
+assert response.status_code == 200, f"/ returned {response.status_code}"
 
 print("All checks passed")
